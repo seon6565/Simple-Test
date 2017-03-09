295877d42cc6a918856b1d7159fceaa69d66adeb
seon6565 <wow2778@naver.com>
2017-03-09 15:03:58 +0900

    수업시간 사과계산까지

--- app/src/main/java/comwow2778/naver/blog/second/Main2Activity.java
+++ app/src/main/java/comwow2778/naver/blog/second/Main2Activity.java
@@ -1,13 +1,36 @@
 package comwow2778.naver.blog.second;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
+    Button b6;
+    EditText e1,e2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        b6 = (Button)findViewById(R.id.b6);
+        e1 = (EditText)findViewById(R.id.e1);
+        e2 = (EditText)findViewById(R.id.e2);
+
+        b6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String price = e1.getText().toString();
+                String count = e2.getText().toString();
+                int i = Integer.parseInt(price);
+                int j = Integer.parseInt(count);
+                Toast.makeText(Main2Activity.this, "가격은"+ i*j, Toast.LENGTH_SHORT).show();
+
+                                  }
+                              }
+        );
     }
 }
--- app/src/main/java/comwow2778/naver/blog/second/MainActivity.java
+++ app/src/main/java/comwow2778/naver/blog/second/MainActivity.java
@@ -7,19 +7,25 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button b1;
+    Button b1, b2, b3,b4,b5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b1 = (Button)findViewById(R.id.button);
+        b1 = (Button)findViewById(R.id.b1);
+        b2 = (Button)findViewById(R.id.b2);
+        b3 = (Button)findViewById(R.id.b3);
+        b4 = (Button)findViewById(R.id.b4);
+        b5 = (Button)findViewById(R.id.b5);
+
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,Main2Activity.class);
-                
+
                 startActivity(intent);
             }
-        });
+        }
+        );
     }
 }
